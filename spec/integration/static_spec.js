8b02b2f3dcd683e5303aaf92c9828d4bc0c88c45
Richard Chan <rchan817@yahoo.com>
2020-12-30 17:00:25 -0800

    refactored back to cjs over esm

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,3 +1,22 @@
-import axios from "axios";
-import server from "../../src/server";
-const base = "http:localhost:3000/";
+const server = require("../../dist/server");
+const axios = require("axios").default;
+const base = "http://localhost:3000/";
+//const base = axios.create({baseURL: "http://localhost:3000"});
+
+describe("routes : static", () => {
+
+  describe("GET /", () => {
+    it("should return status code 200", (done) => {
+      axios.get(base)
+      .then((res) => {
+        expect(res.status).toBe(200);
+        done();
+      })
+      .catch((err) => {
+        console.log(err);
+        done();
+      })
+    })
+  })
+
+});
